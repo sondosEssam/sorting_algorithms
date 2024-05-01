04ae4326a987a7ed0e10f8d56c827f02b3e51b1d
SondosEssam <sondosessam713@gmail.com>
2024-05-01 05:09:01 +0300

    ds

--- sort.h
+++ sort.h
@@ -22,4 +22,6 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void quick_sort(int *array, size_t size);
+int partition(int *array, int low, int high);
 #endif /* MAIN_H */
