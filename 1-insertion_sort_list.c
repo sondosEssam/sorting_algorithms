4c04df79077da2191afd975b1a5227702b4bf5b6
SondosEssam <sondosessam713@gmail.com>
2024-05-01 04:46:42 +0300

    insert

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - function
+ *@list: arr
+ *Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *i = NULL, *j = NULL, *save = NULL, *curr = *list;
+	int key = 0;
+
+	while (curr != NULL)
+	{
+		save = curr->next;
+		i = curr->prev;
+		while (i != NULL && i->n > curr->n)
+		{
+		j = i->prev;
+		if (j == NULL)
+		{
+			curr->next->prev = i;
+			(i)->next = (curr)->next;
+			(i)->prev = curr;
+			(curr)->prev = NULL;
+			(curr)->next = (i);
+			(*list) = curr;
+		}
+		else
+		{
+			i->next = curr->next;
+			if (curr->next != NULL)
+				curr->next->prev = i;
+			i->prev = curr;
+			j->next = curr;
+			curr->prev = j;
+			curr->next = i;
+		}
+		 i = j;
+		print_list(*list);
+		}
+	curr = save;
+	}
+}
