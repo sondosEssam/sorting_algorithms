8d9c8b30e86b14be316a6307f63f71c510591771
SondosEssam <sondosessam713@gmail.com>
2024-05-01 19:19:33 +0300

    sa

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,40 +6,43 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *i = NULL, *j = NULL, *save = NULL, *curr = *list;
+	listint_t *i = NULL, *j = NULL, *save = NULL, *curr = NULL;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (list == NULL)
 		return;
+	if (*list == NULL || (*list)->next == NULL)
+		return;
+	curr = *list;
 	while (curr != NULL)
 	{
 		save = curr->next;
 		i = curr->prev;
 		while (i != NULL && i->n > curr->n)
 		{
-		j = i->prev;
-		if (j == NULL)
-		{
-			if (curr->next != NULL)
-				curr->next->prev = i;
-			(i)->next = (curr)->next;
-			(i)->prev = curr;
-			(curr)->prev = NULL;
-			(curr)->next = (i);
-			(*list) = curr;
+			j = i->prev;
+			if (j == NULL)
+			{
+				if (curr->next != NULL)
+					curr->next->prev = i;
+				(i)->next = (curr)->next;
+				(i)->prev = curr;
+				(curr)->prev = NULL;
+				(curr)->next = (i);
+				(*list) = curr;
+			}
+			else
+			{
+				i->next = curr->next;
+				if (curr->next != NULL)
+					curr->next->prev = i;
+				i->prev = curr;
+				j->next = curr;
+				curr->prev = j;
+				curr->next = i;
+			}
+			i = j;
+			print_list(*list);
 		}
-		else
-		{
-			i->next = curr->next;
-			if (curr->next != NULL)
-				curr->next->prev = i;
-			i->prev = curr;
-			j->next = curr;
-			curr->prev = j;
-			curr->next = i;
-		}
-		 i = j;
-		print_list(*list);
-		}
-	curr = save;
+		curr = save;
 	}
 }
