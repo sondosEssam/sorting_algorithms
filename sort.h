00b01416f1e873752b49a6ac5139d90c6dfa3ef1
SondosEssam <sondosessam713@gmail.com>
2024-05-01 19:53:39 +0300

    new

--- sort.h
+++ sort.h
@@ -23,5 +23,6 @@ void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void quick_sort(int *array, size_t size);
-int partition(int *array, int low, int high);
+int partition(int *array, int low, int high, size_t size);
+void quick_sort_recursion(int *array, int low, int high, size_t size);
 #endif /* MAIN_H */
